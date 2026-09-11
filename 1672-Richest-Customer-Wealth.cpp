15b9ee0be549064730167c2e775e81b21827e8b3
JASWAN KUMAR .K <jaswan.aug@gmail.com>
2026-09-11 12:27:38 +0530

    [1672] [Time Beats: 100.00%] [Memory Beats: 55.38%] - LeetPush

---
+++ 1672-Richest-Customer-Wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int rich=0;
+        for(int i=0;i<accounts.size();i++)
+        {
+            int wealth=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                wealth+=accounts[i][j];
+            }
+            rich=max(rich,wealth);
+        }
+        return rich;
+    }
+};
