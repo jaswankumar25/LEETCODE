5a5197b075a6154fd96fbd9da51726136e368893
JASWAN KUMAR .K <jaswan.aug@gmail.com>
2026-09-23 10:03:31 +0530

    [1038] [Time Beats: 100.00%] [Memory Beats: 49.37%] - LeetPush

---
+++ 1038-Binary-Search-Tree-to-Greater-Sum-Tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sum = 0;
+    void reverseInorder(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
+        reverseInorder(root->right);
+        sum+=root->val;
+        root->val=sum;
+        reverseInorder(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        reverseInorder(root);
+        return root;
+    }
+};
